91e2153905a8a087fe6d44fbb660b330b507de3e
Ryan L. Boyd <ryanb@ryanb.cc>
2020-09-10 15:00:15 +0100

    styled Gui to GUI

--- igorGui/Properties/AssemblyInfo.cs
+++ igorGui/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("igorGui")]
+[assembly: AssemblyTitle("igorGUI")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("igorGui")]
+[assembly: AssemblyProduct("igorGUI")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- igorGui/Properties/Resources.Designer.cs
+++ igorGui/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace igorGui.Properties {
+namespace igorGUI.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace igorGui.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("igorGui.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("igorGUI.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
